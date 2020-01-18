f2d1fe07e0849cf3d26b284a48e035a3ba3ab831
wellcampos <wgt.conceicao.campos@gmail.com>
2020-01-18 00:13:53 -0300

    comit

--- EnCAD_Label/Program.cs
+++ EnCAD_Label/Program.cs
@@ -16,7 +16,7 @@ namespace EnCAD_Label
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmPrincipal());
         }
     }
 }
